af95ceb4de9197695ca5183e1dc39725d485a7b4
Marchinka90 <martin_marchev@abv.bg>
2024-03-12 10:14:06 +0200

    Refactor SWR Config

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,21 +1,27 @@
+"use client";
+
+import { SWRConfig } from "swr";
 import Footer from "./footer";
 import Header from "./header";
 import NavBar from "./navbar";
+import fetcher from "../util/fetcher";
 
 export default function PrivateLayout({
   children,
 }: {
-  
+
   children: React.ReactNode;
 }) {
   return (
-    <div>
-      <Header />
-      <NavBar />
-      <main>
-        {children}
-      </main>
-      <Footer />
-    </div>
+    <SWRConfig value={{ fetcher: fetcher }} >
+      <div>
+        <Header />
+        <NavBar />
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </SWRConfig>
   )
 }
