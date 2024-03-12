58a00511c6e31b2e22121b51f2f8e7938559842e
Marchinka90 <martin_marchev@abv.bg>
2024-03-12 18:42:15 +0200

    Added Header and User Component

--- app/(private)/header.tsx
+++ app/(private)/header.tsx
@@ -1,5 +1,6 @@
 'use client';
 import useSWR from "swr";
+import User from "../components/user";
 
 export default function Header() {
   const { data, error, isLoading } = useSWR('/api/users/profile');
@@ -19,8 +20,13 @@ export default function Header() {
   console.log(data);
 
   return (
-    <header>
-      Header
+    <header className="flex flex-row w-full p-5 bg-slate-800 rounded-lg my-2 justify-between items-center">
+      <div>
+        <h1 className="font-mono text-lg">Twitter Prototype</h1>
+      </div>
+      <div>
+        <User user={data.data} href="account"/>
+      </div>
     </header>
   )
 }
