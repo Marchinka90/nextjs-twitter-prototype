1b635675dd8ad392ac69c1037cfff60a27ee7b09
Marchinka90 <martin_marchev@abv.bg>
2024-03-13 16:16:48 +0200

    Added Update Post Endpoint

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -16,3 +16,22 @@ export async function GET(
 
   return NextResponse.json({ data: res.rows[0] });
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+  const jwtPayload = await getJWTPayload();
+  const res = await sql(`select from posts where id = $1 and user_id = $2`,
+    [params.id, jwtPayload.sub]);
+
+  if (res.rowCount == 0) {
+    return NextResponse.json({ error: 'Not Found' }, { status: 404 });
+  }
+
+  await sql(`update posts set content = $1 where id = $1 and user_id = $2`,
+    [body.content, params.id, jwtPayload.sub]);
+
+  return NextResponse.json({ msg: 'Update Succes' });
+}
